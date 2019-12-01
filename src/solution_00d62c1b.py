1dec1cb10e416eae29bddde961c4fcaa4468fbb0
Saibhe <saibhe.mccullough@fireeye.com>
2019-12-01 15:33:47 +0000

    Edit comments on solution solution_00d62c1b.py

--- src/solution_00d62c1b.py
+++ src/solution_00d62c1b.py
@@ -20,60 +20,38 @@ def solve():
     #locate the 3 values, and save them as result
     result = np.where(input == 3)
 
-    #put the result values together so that they become a list of coordinates. 
+    #put the result values together so that they become a list of coordinates (grid locations). 
     #These coordinates can be used to locate where the 3 values are in the grid 
     listOfCoordinates= list(zip(result[1], result[2]))
-    listOfCoordinates
-
-    #i = 0
-    #while i <=len(listOfCoordinates):
-    current_coord = listOfCoordinates[0]
-        #next_coord = listOfCoordinates[i+1]
-    print ('current', current_coord)
-        #print ('next', next_coord)
-        #pattern = current_cord[]
-    current_coord
-    #current_coord[0][0]
-    #current_coord[0][0]
-    listOfCoordinates[2][0] 
-
-    print(current_coord[1])
-    x = current_coord[0]
-    y = current_coord[1]
-    print (x,y)
-
+   
+    #iterate through the list of co-ordinates
     for coord in listOfCoordinates:
-        current = coord
-        
-        #check for 3 in next row down, to the left
+     
+        #check for a 3 in next row down, to the left
         if coord[0] == (x+1): 
-           # print ('x left')
             if coord[1] < y:
-              #  print ('Y left')
-                print ('left', coord)
+               #assign co-ordinate to the variable 'left'
                 left = coord
                 
-                #check for 3 on this row, to the right
+                #check for 3 on next row down from current co-oordiante
                 for coord in listOfCoordinates:
                     if coord[1] > y:
-                      #  print ('Y on the right also')
-                        print ('right', coord)
+                        #assign co-ordinate to the variable 'right'
                         right = coord
+
+                        #check for 3 on next row down from current co-oordiante
                         for coord in listOfCoordinates:
                             if coord[0] == (x+2):
-                                #print ('x on the bottom')
                                 if coord[1] == y:
-                                   # print ('Y on the bottom also')
-                                    print ('bottom', coord)
+                                    #assign co-ordinate to the variable 'bottom'
                                     bottom = coord
-                                    print (left, right, bottom)
-                                    
-                                    #need to find all locations in between current, left, right, bottom, and fill them in with a number
-                
-                
-                
+
+                                    #in the case that there is green squares, surrounding 1 or more black square
+                                    #the program needs to change the value of the sqaure, so that it is yellow
+                                           
             
         else:
-            print ('none found')
+            #if there is no green sqaures to create the shape, do nothing
+            pass
 
 solve()            
